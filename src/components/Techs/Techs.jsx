bc92b062a318f99053dbae4407a96a9c2aa113ad
joseu <joseu@DESKTOP-4GF1JQ4>
2023-10-11 19:29:34 -0500

    styles

--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -4,16 +4,16 @@ import pic from "../../assets/ratge.png";
 export const Techs = () => {
     return (
         <div className={css.container}>
-            <h1>Skills</h1>
+            <h1 className={css.title}>Skills</h1>
             <div className={css.contGrid}>
                 <div className={css.techSpace}>
-                    <h2>Skills</h2>
+                    <h2 className={css.subtitle}>Frontend</h2>
                 </div>
                 <div className={css.techSpace}>
-                    <h2>Skills</h2>
+                    <h2 className={css.subtitle}>Backend</h2>
                 </div>
                 <div className={css.techSpaceCenter}>
-                    <h2>Skills</h2>
+                    <h2 className={css.subtitle}>Others</h2>
                 </div>
             </div>
         </div>
