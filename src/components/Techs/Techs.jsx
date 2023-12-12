80cb09a1c868792082d1a9036d9beb098023effe
joseu <joseu@DESKTOP-4GF1JQ4>
2023-12-12 15:22:38 -0500

    techsDesign

--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -7,62 +7,29 @@ export const Techs = () => {
             <h1 className={css.title}>Techs</h1>
             <div className={css.contFlex}>
                 <div className={css.techSpace}>
-                    <h2 className={css.subtitle}>Frontend</h2>
+                    <h2 className={css.subtitle}>Front-End</h2>
                     <section className={css.techs}>
-                    <div className={css.mini}>
-                        <img id="React" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"/>
-                        <b className={css.names}>React</b>
-                    </div>
-                    <div className={css.mini}>
-                        <img id="Redux" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"/>
-                        <b className={css.names}>Redux</b> 
-                    </div>
-                    <div className={css.mini}>
-                        <img id="HTML" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"/>
-                        <b className={css.names}>HTML</b> 
-                    </div>
-                    <div className={css.mini}>
-                        <img id="CSS" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"/>
-                        <b className={css.names}>CSS</b> 
-                    </div>
-                    <div className={css.mini}>
-                        <img id="JavaScript" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"/>
-                        <b className={css.names}>JavaScript</b> 
-                    </div>
-                    <div className={css.mini}>
-                        <img id="Bootstrap" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"/>
-                        <b className={css.names}>Bootstrap</b> 
-                    </div>
+                        <img id="React" title="React" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"/>
+                        <img id="Redux" title="Redux" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"/>
+                        <img id="HTML" title="HTML" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"/>
+                        <img id="CSS" title="CSS" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"/>
+                        <img id="JavaScript" title="JavaScript" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"/>
+                        <img id="Bootstrap" title="Bootstrap" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"/>
                     </section>
                 </div>
                 <div className={css.techSpace}>
-                    <h2 className={css.subtitle}>Backend</h2>
+                    <h2 className={css.subtitle}>Back-End</h2>
                     <section className={css.techs}>
-                    <div className={css.mini}>
-                        <img id="Node.js" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"/>
-                        <b className={css.names}>Node Js</b> 
-                    </div>
-                    <div className={css.mini}>
-                        <img id="Express" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"/>
-                        <b className={css.names}>Express</b> 
-                    </div>
-                    <div className={css.mini}>
-                        <img id="PostgreSQL" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"/>
-                        <b className={css.names}>PostgreSQL</b> 
-                    </div>
+                        <img id="Node.js" title="Node.js"className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"/>
+                        <img id="Express" title="Express" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"/>
+                        <img id="PostgreSQL" title="PostgreSQL" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"/>
                     </section>
                 </div>
                 <div className={css.techSpace}>
                     <h2 className={css.subtitle}>Others</h2>
                     <section className={css.techs}>
-                    <div className={css.mini}>
-                        <img id="Git" className={css.icons} src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"/>
-                        <b className={css.names}>Git</b> 
-                    </div>
-                    <div className={css.mini}>
-                        <img id="Photoshop" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg"/>
-                        <b className={css.names}>Photoshop</b> 
-                    </div>
+                        <img id="Git" title="Git" className={css.icons} src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"/>
+                        <img id="Photoshop" title="Photoshop" className={css.icons} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg"/>
                     </section>
                 </div>
             </div>
