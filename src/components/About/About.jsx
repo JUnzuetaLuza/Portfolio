9fa87a7f2f7fda45669175c066a9656dcb65bc95
joseu <joseu@DESKTOP-4GF1JQ4>
2023-12-19 09:12:11 -0500

    AboutTexts

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,16 @@
 import css from "./About.module.css";
 import pic from "../../assets/ratge.png";
-import { useMediaQuery } from 'react-responsive';
 
 export const About = () => {
-    const isMobile = useMediaQuery({ maxWidth: 767 });
-
     return (
         <div className={css.container}>
             <div className={css.leftAbout}>
-                <h1 className={css.title}>José Unzueta</h1>
-                <h2 className={css.subtitle}>Full Stack Developer</h2>
+                <h2 className={css.subtitle}>Hi, i'm José Unzueta</h2>
+                <h1 className={css.title}>Full Stack Developer</h1>
                 <br/>
-                <span className={css.text}>I'm a passionate full stack web developer with experience in a variety of technologies. My goal is to continue learning and growing in this exciting industry while contributing to the development of innovative solutions. I am committed to excellence and continuous improvement, and am excited about the opportunities this ever-evolving industry has to offer. I am always willing to take on new challenges and am eager to contribute to projects that require a creative mind and solid technical skills.</span>
+                <h3 className={css.text}>Lets make your dream web</h3>
             </div>
-            <div className={css.rightAbout}>
-                <img className={css.imgProfile} src={pic}/>
-            </div>
-            
+            <img className={css.imgProfile} src={pic}/>
         </div>
     )
 }
